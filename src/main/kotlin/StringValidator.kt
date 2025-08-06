86bbf8ffcac2e0d5f0be682340d979298306c3ca
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-08-06 03:34:43 -0600

    Create StringValidator.kt

---
+++ src/main/kotlin/StringValidator.kt
@@ -0,0 +1,18 @@
+class StringValidator(
+    private var strategy: ValidationStrategy
+) {
+    fun setStrategy(strategy: ValidationStrategy) {
+        this.strategy = strategy
+    }
+
+    fun isValid(input: String): Boolean {
+        val machine = strategy.machine
+        if (!strategy.passesChecks(input)) return false
+        for (char in input) {
+            val symbol = char.toString()
+            machine.processSymbol(symbol)
+            if (machine.isInvalid) return false
+        }
+        return machine.getResult()
+    }
+}
