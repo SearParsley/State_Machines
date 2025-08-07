d8aea2b609bd0739a03d8ba679e5f272bc331c34
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-08-07 00:49:13 -0600

    Create EmailStrategy.kt

---
+++ src/main/kotlin/EmailStrategy.kt
@@ -0,0 +1,63 @@
+class EmailStrategy : ValidationStrategy {
+    override val machine = EmailStateMachine()
+}
+
+class EmailStateMachine : StateMachine(EmailStartState()) {
+    override fun getResult(): Boolean = currentState is EmailAcceptState
+}
+
+class EmailStartState : State() {
+    override fun processSymbol(symbol: String): State {
+        return when (symbol) {
+            in ".@" -> InvalidState()
+            else -> EmailPartOneState()
+        }
+    }
+}
+
+class EmailPartOneState : State() {
+    override fun processSymbol(symbol: String): State {
+        return when (symbol) {
+            in "@" -> EmailAtSymbolState()
+            in "." -> InvalidState()
+            else -> EmailPartOneState()
+        }
+    }
+}
+
+class EmailAtSymbolState : State() {
+    override fun processSymbol(symbol: String): State {
+        return when (symbol) {
+            in ".@" -> InvalidState()
+            else -> EmailPartTwoState()
+        }
+    }
+}
+
+class EmailPartTwoState : State() {
+    override fun processSymbol(symbol: String): State {
+        return when (symbol) {
+            in "." -> EmailPeriodState()
+            in "@" -> InvalidState()
+            else -> EmailPartTwoState()
+        }
+    }
+}
+
+class EmailPeriodState : State() {
+    override fun processSymbol(symbol: String): State {
+        return when (symbol) {
+            in ".@" -> InvalidState()
+            else -> EmailAcceptState()
+        }
+    }
+}
+
+class EmailAcceptState : State() {
+    override fun processSymbol(symbol: String): State {
+        return when (symbol) {
+            in ".@" -> InvalidState()
+            else -> EmailAcceptState()
+        }
+    }
+}
